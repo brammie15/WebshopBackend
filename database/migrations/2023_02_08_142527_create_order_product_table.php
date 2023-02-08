dc63ba1b99e9fb3b66598f8f1e6a892eda38ae7e
brammie15 <Brammieverhulst@gmail.com>
2023-02-08 20:43:58 +0100

    qdd qaelfe

---
+++ database/migrations/2023_02_08_142527_create_order_product_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_product', function (Blueprint $table) {
+            $table->id();
+            $table->integer('order_id')->unsigned();
+            $table->integer('product_id')->unsigned();
+            $table->foreign('order_id')->references('id')->on('order')->onDelete('cascade');
+            $table->foreign('product_id')->references('id')->on('product')->onDelete('cascade');
+//            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_product');
+    }
+};
